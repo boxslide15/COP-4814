2376ec2af967d23362c77f1c62a7ef108dbfbc3a
Luis Hernandez <luisfhernandez415@gmail.com>
2019-03-04 20:59:30 -0500

    Small Change

--- Assignment 5/aqiProject.py
+++ Assignment 5/aqiProject.py
@@ -7,7 +7,6 @@ def save_to_file(filename, data):
         json.dump(data, write_file, indent=4)
         print("The file {0} was successfully created.".format(filename))
 
-
 def read_from_file(filename):
     with open(filename, "r") as read_file:
         jsonData = json.load(read_file)
